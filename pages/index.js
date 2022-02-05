36547fe0d286042ce449d8314a8115fe8daa3282
박도희 <dohee.pa@gmail.com>
2022-02-05 23:01:59 +0900

    2.1 Fetching Data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react';
 import Seo from '../components/Seo';
 
+const API_KEY = 'f94110cdc75b4785072b4b525f15fb53';
+
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      )).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <Seo title='Home' />
-      <h1 className='yellow'>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      { movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
