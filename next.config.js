e06555eb00fc1311f6f2380db52d0df690b18c25
박도희 <dohee.pa@gmail.com>
2022-02-05 23:41:44 +0900

    2.2 Redirect and Rewrite

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+      return [{
+        source: `/api/movies`,
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      }]
+  },
 }
